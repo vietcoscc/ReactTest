ac3abd8f7eb1082ea31402dd9a17c3459d1fe284
VietNQ <vietcoscc@gmail.com>
2021-04-15 15:10:24 +0700

    Add comment

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,10 @@
 import App from './App';
 import React from "react";
-import {fireEvent, render, screen, waitFor} from "@testing-library/react";
+import {fireEvent, render, screen, waitFor, cleanup} from "@testing-library/react";
 
 const oldWindowLocation = window.location;
 beforeAll(() => {
     delete window.location;
-
     window.location = Object.defineProperties(
         {},
         {
@@ -24,26 +23,27 @@ afterAll(() => {
     // restore `window.location` to the `jsdom` `Location` object
     window.location = oldWindowLocation
 });
+afterEach(cleanup);
 describe('App', () => {
     test('Full flow test', async () => {
         // Render all component
         render(<App/>);
 
-        //query title tag
+        //Check display home title
         const homeTitle = screen.queryByText("Image list");
         expect(homeTitle).toBeInTheDocument();
 
 
-        //Wait for API call to display item
+        //Wait for image API call
         await waitFor(async () => {
-            //find all image tag
             const imageItems = await screen.findAllByTestId("image-item");
+            //Check display images
             imageItems.forEach(item => {
                 expect(item).toBeInTheDocument()
             })
         }, {timeout: 3000});
 
-        //query item see more
+        //Check display See more button
         const seeMore = screen.queryByTestId("see-more");
         expect(seeMore).toHaveTextContent("See more");
 
@@ -51,64 +51,77 @@ describe('App', () => {
         const imageItems = await screen.findAllByTestId("image-item");
         //Query button see more
         const btnSeeMore = screen.queryByTestId("see-more");
-        //Emulate see more click
+        //Simulate See more click
         fireEvent.click(btnSeeMore);
+        //Wait for image API call
         await waitFor(async () => {
-
             const afterClickItems = await screen.findAllByTestId("image-item");
+            //Check display images
             expect(afterClickItems.length).toBe(imageItems.length + 3)
         }, {timeout: 3000});
 
-        //Emulate click on image item
+        //Simulate click on image item
         const images = await screen.findAllByTestId("image");
         expect(images).toBeTruthy();
         fireEvent.click(images[0]);
 
-        //Detail
+        //Check display Detail screen
         const detailTitle = screen.queryByTestId("detail-title");
         expect(detailTitle).toBeInTheDocument();
         expect(detailTitle).toHaveTextContent("Detail");
 
+        //Check image info
         const numberRegex = new RegExp('^[0-9]+$');
 
+        //Check ID
         const imageId = screen.queryByTestId("image-id");
         expect(imageId).toBeInTheDocument();
         expect(imageId.innerHTML.toString()).toMatch(numberRegex);
 
+        //Check Width
         const imageWidth = screen.queryByTestId("image-width");
         expect(imageWidth).toBeInTheDocument();
         expect(imageWidth.innerHTML.toString()).toMatch(numberRegex);
 
+        //Check Height
         const imageHeight = screen.queryByTestId("image-height");
         expect(imageHeight).toBeInTheDocument();
         expect(imageWidth.innerHTML.toString()).toMatch(numberRegex);
 
+
+        //Check Image URL
         const imageUrl = screen.queryByTestId("image-url");
         expect(imageUrl).toBeInTheDocument();
-        const urlRegex = new RegExp("https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)")
+        const urlRegex = new RegExp("https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)");
         expect(imageUrl.innerHTML.toString()).toMatch(urlRegex);
 
+        //Check Image photographer
         const imagePhotographer = screen.queryByTestId("image-photographer");
         expect(imagePhotographer).toBeInTheDocument();
 
+        //Check Image photographer URL
         const imagePhotographerUrl = screen.queryByTestId("image-photographer_url");
         expect(imagePhotographerUrl).toBeInTheDocument();
 
+        //Check Image photographer ID
         const imagePhotographerId = screen.queryByTestId("image-photographer_id");
         expect(imagePhotographerId).toBeInTheDocument();
 
+        //Check Image Avg color
         const hexColorCodeRegex = new RegExp("^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$");
         const imageAvgColor = screen.queryByTestId("image-avg_color");
         expect(imageAvgColor).toBeInTheDocument();
         expect(imageAvgColor.innerHTML.toString()).toMatch(hexColorCodeRegex);
 
+        //Check Image liked
         const imageLiked = screen.queryByTestId("image-liked");
         expect(imageLiked).toBeInTheDocument();
         expect(imageLiked).toHaveTextContent('false')
 
+        //Simulate image click
         const displayImage = screen.queryByTestId("display-image");
-        console.log(displayImage);
         fireEvent.click(displayImage);
+        //Check function call
         expect(window.location.assign).toBeCalledTimes(1);
     });
 
